7355146b8638d63de2aae8bc5e8661f4c62ba043
3408885374@qq.com <3408885374@qq.com>
2026-06-05 11:12:02 +0800

    修复启动类包扫描，加载Controller

--- src/main/java/com/example/frontstudy/FrontStudyApplication.java
+++ src/main/java/com/example/frontstudy/FrontStudyApplication.java
@@ -1,11 +1,13 @@
 package com.example.frontstudy;
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// 关键：扫描com.example.demo全包 + Mapper扫描
+@SpringBootApplication(scanBasePackages = "com.example.demo")
+@MapperScan("com.example.demo.mapper")
 public class FrontStudyApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FrontStudyApplication.class, args);
+        SpringApplication.run(FrontStudyApplication.class,args);
     }
 }
