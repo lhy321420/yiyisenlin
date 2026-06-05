62d4f32bc227b724b4a668f74b8a18b81f33515c
3408885374@qq.com <3408885374@qq.com>
2026-06-05 15:01:55 +0800

    精准放

--- src/main/java/com/example/demo/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/demo/interceptor/LoginInterceptor.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
+
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 放行OPTIONS请求（CORS预检）
+        System.out.println("=== 拦截器捕获请求: " + request.getRequestURI() + " ===");
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             return true;
         }
